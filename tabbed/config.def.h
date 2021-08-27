981ecf13700052fabb3fb8d84e75b6af658c1181
Alexander Neville <alexander.neville@icloud.com>
2021-08-27 15:33:48 +0100

    applying patches

--- tabbed/config.def.h
+++ tabbed/config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
-static const char* normbgcolor  = "#222222";
-static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
-static const char* urgbgcolor   = "#111111";
-static const char* urgfgcolor   = "#cc0000";
+static const char font[]        = "Jetbrains Mono Nerd Font:size=10";
+static const char* normbgcolor  = "#353c4a";
+static const char* normfgcolor  = "#d8dee9";
+static const char* selbgcolor   = "#81a1c1";
+static const char* selfgcolor   = "#2e3440";
+static const char* urgbgcolor   = "#2e3440";
+static const char* urgfgcolor   = "#bf616a";
 static const char before[]      = "<";
 static const char after[]       = ">";
 static const char titletrim[]   = "...";
@@ -36,13 +36,12 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
-
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY,               XK_t,      focusonce,   { 0 } },
+	{ MODKEY,               XK_t,      spawn,       { 0 } },
+	{ MODKEY|ShiftMask,     XK_j,      rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_k,      rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_h,      movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_l,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -63,4 +62,13 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+
+	{ MODKEY,               XK_Shift_L, showbar,    { .i = 1 } },
+	{ ShiftMask,            XK_Control_L, showbar,    { .i = 1 } },
+};
+
+static Key keyreleases[] = {
+	/* modifier             key          function     argument */
+	{ MODKEY|ShiftMask,     XK_Shift_L,  showbar,     { .i = 0 } },
+	{ MODKEY|ShiftMask,     XK_Control_L,  showbar,     { .i = 0 } },
 };
